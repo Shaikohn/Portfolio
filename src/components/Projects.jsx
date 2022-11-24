951b8c413ce2b949b45fcda6b731fadcfbab0ef0
unknown <shaicokohn@gmail.com>
2022-11-24 20:38:36 -0300

    Tourism App added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Feetsies from '../assets/projects/Feetsies1.png'
 import DogApp from '../assets/projects/DogApp.png'
+import Cordoba from '../assets/projects/Cordoba.png'
 
 const Projects = () => {
     return (
@@ -24,7 +25,7 @@ const Projects = () => {
                                 <a href='https://pfgrupo3.vercel.app/' target="_blank" rel="noreferrer">
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                                 </a>
-                                <a href='https://github.com/PCardozo/pfgrupo3' target="_blank" rel="noreferrer">
+                                <a href='https://github.com/Shaikohn/Feetsies' target="_blank" rel="noreferrer">
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                                 </a>
                             </div>
@@ -43,6 +44,24 @@ const Projects = () => {
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                                 </a>
                                 <a href='https://github.com/Shaikohn/Dogs-App' target="_blank" rel="noreferrer">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Grid Item Tourism */}
+                    <div style={{backgroundImage: `url(${Cordoba})`}}
+                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                Tourism App (HTML)
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href='https://tourism-app-kohn-giacobbe.vercel.app/index.html' target="_blank" rel="noreferrer">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                </a>
+                                <a href='https://github.com/Shaikohn/Tourism-App' target="_blank" rel="noreferrer">
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                                 </a>
                             </div>
