2705a307d55e3968f6bd73cc361a0d8d3867e09e
unknown <shaicokohn@gmail.com>
2023-01-19 12:47:16 -0300

    Added projects and skills

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Feetsies from '../assets/projects/Feetsies1.png'
 import DogApp from '../assets/projects/DogApp.png'
 import Cordoba from '../assets/projects/Cordoba.png'
+import Portfolio from '../assets/projects/Portfolio.png'
+import Ztream from '../assets/projects/Ztream.png'
+import Cuevanix from '../assets/projects/Cuevanix.jpg'
 
 const Projects = () => {
     return (
@@ -18,7 +21,7 @@ const Projects = () => {
                     className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                         {/* Hover Effects */}
                         <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
+                            <span className='text-2xl font-bold text-white tracking-wider text-align-center'>
                                 Feetsies Application
                             </span>
                             <div className='pt-8 text-center'>
@@ -26,6 +29,42 @@ const Projects = () => {
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                                 </a>
                                 <a href='https://github.com/Shaikohn/Feetsies' target="_blank" rel="noreferrer">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Grid Item Ztreamgames */}
+                    <div style={{backgroundImage: `url(${Ztream})`}}
+                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                Ztreamgames
+                            </span>
+                            <div className='pt-8 text-center'>
+                                {/* <a href='https://feetsies.vercel.app/' target="_blank" rel="noreferrer">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                </a> */}
+                                <a href='https://github.com/Shaikohn/Zteam' target="_blank" rel="noreferrer">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Grid Item Cuevanix */}
+                    <div style={{backgroundImage: `url(${Cuevanix})`}}
+                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                Cuevanix
+                            </span>
+                            <div className='pt-8 text-center'>
+                                {/* <a href='https://feetsies.vercel.app/' target="_blank" rel="noreferrer">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                </a> */}
+                                <a href='https://github.com/Shaikohn/Cuevanix' target="_blank" rel="noreferrer">
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                                 </a>
                             </div>
@@ -44,6 +83,21 @@ const Projects = () => {
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                                 </a>
                                 <a href='https://github.com/Shaikohn/Dogs-App' target="_blank" rel="noreferrer">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Grid Item Portfolio */}
+                    <div style={{backgroundImage: `url(${Portfolio})`}}
+                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                This portfolio
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href='https://github.com/Shaikohn/Portfolio' target="_blank" rel="noreferrer">
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                                 </a>
                             </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,10 +6,11 @@ import ReactImg from '../assets/react.png';
 import Node from '../assets/node.png';
 /* import FireBase from '../assets/firebase.png'; */
 import GitHub from '../assets/github.png';
-/* import Tailwind from '../assets/tailwind.png';
-import Mongo from '../assets/mongo.png'; */
+/* import Tailwind from '../assets/tailwind.png'; */
+import Mongo from '../assets/mongo.png'; 
 import Redux from '../assets/Redux.png';
 import postgres from '../assets/postgres.png';
+import Bootstrap from '../assets/Bootstrap.png';
 
 const Skills = () => {
     return (
@@ -22,36 +23,64 @@ const Skills = () => {
                 </div>
                 <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img alt="HTML" src={HTML} className='w-20 mx-auto' />
-                        <p className='my-4'>HTML</p>
+                        <a href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5" target="_blank" rel="noopener noreferrer">
+                            <img alt="HTML" src={HTML} className='w-20 mx-auto' />
+                            <p className='my-4'>HTML</p>
+                        </a>
                     </div>
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img alt="CSS" src={CSS} className='w-20 mx-auto' />
-                        <p className='my-4'>CSS</p>
+                        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank" rel="noopener noreferrer">
+                            <img alt="CSS" src={CSS} className='w-20 mx-auto' />
+                            <p className='my-4'>CSS</p>
+                        </a>
                     </div>
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img alt="Javascript" src={JavaScript} className='w-20 mx-auto' />
-                        <p className='my-4'>JAVASCRIPT</p>
+                        <a href="https://www.javascript.com/" target="_blank" rel="noopener noreferrer">
+                            <img alt="Javascript" src={JavaScript} className='w-20 mx-auto' />
+                            <p className='my-4'>JAVASCRIPT</p>
+                        </a>
                     </div>
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img alt="React" src={ReactImg} className='w-20 mx-auto' />
-                        <p className='my-4'>REACT</p>
+                        <a href="https://es.reactjs.org/" target="_blank" rel="noopener noreferrer">
+                            <img alt="React" src={ReactImg} className='w-20 mx-auto' />
+                            <p className='my-4'>REACT</p>
+                        </a>
                     </div>
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img alt="Redux" src={Redux} className='w-20 mx-auto' />
-                        <p className='my-4'>REDUX</p>
+                        <a href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
+                            <img alt="Redux" src={Redux} className='w-20 mx-auto' />
+                            <p className='my-4'>REDUX</p>
+                        </a>
                     </div>
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img alt="Node" src={Node} className='w-20 mx-auto' />
-                        <p className='my-4'>NODE JS</p>
+                        <a href="https://nodejs.org/es/" target="_blank" rel="noopener noreferrer">
+                            <img alt="Node" src={Node} className='w-20 mx-auto' />
+                            <p className='my-4'>NODE JS</p>
+                        </a>
                     </div>
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                        <a href="https://www.mongodb.com/cloud/atlas/lp/try4?utm_source=google&utm_campaign=search_gs_pl_evergreen_atlas_core_prosp-brand_gic-null_amers-ar_ps-all_desktop_eng_lead&utm_term=mongodb&utm_medium=cpc_paid_search&utm_ad=e&utm_ad_campaign_id=12212624305&adgroup=115749712783&gclid=CjwKCAiAqt-dBhBcEiwATw-ggCgcronQDx8z_aU5xfOVieuODiayP0BQC_zXGFrTHUNVODUEthzDUhoCZNMQAvD_BwE" target="_blank" rel="noopener noreferrer">
+                            <img alt="MongoDB" src={Mongo} className='w-20 mx-auto' />
+                            <p className='my-4'>MONGO DB</p>
+                        </a>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                        <a href="https://www.postgresql.org/" target="_blank" rel="noopener noreferrer">
                         <img alt="Postgres" src={postgres} className='w-20 mx-auto' />
                         <p className='my-4'>POSTGRES SQL</p>
+                        </a>
                     </div>
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img alt="Github" src={GitHub} className='w-20 mx-auto' />
-                        <p className='my-4'>GITHUB</p>
+                        <a href="https://github.com/Shaikohn" target="_blank" rel="noopener noreferrer">
+                            <img alt="Github" src={GitHub} className='w-20 mx-auto' />
+                            <p className='my-4'>GITHUB</p>
+                        </a>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                        <a href="https://getbootstrap.com/" target="_blank" rel="noopener noreferrer">
+                            <img alt="Bootstrap" src={Bootstrap} className='w-20 mx-auto' />
+                            <p className='my-4'>BOOTSTRAP</p>
+                        </a>
                     </div>
                 </div>
             </div>
